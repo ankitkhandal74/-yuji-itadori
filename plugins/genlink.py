1d347c08b9d23cfee43161e9e3b2b8202164bd95
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2024-05-07 00:37:58 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -34,10 +34,12 @@ async def gen_link_s(bot, message):
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    btn = InlineKeyboardButton('⚡ ᴅᴏᴡɴʟᴏᴀᴅ LINK 🔗 ⚡', url=https://t.me/{temp.U_NAME}?start={outstr}
-    reply_markup = InlineKeyboardMarkup(btn)
-    
-    await message.reply(f"Your link /n",reply_markup=reply_markup)
+    button_text = "Click Here"
+    button_url = f"https://t.me/{temp.U_NAME}?start={outstr}"
+    button = InlineKeyboardButton(button_text, url=button_url)
+    reply_markup = InlineKeyboardMarkup([[button]])
+
+    await message.reply(text="Here is your Link:", reply_markup=reply_markup)
     
     
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
