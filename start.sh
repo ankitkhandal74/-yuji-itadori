f8523dca429e55319f31c2c84c3a10c48ea6b514
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2024-05-30 09:49:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot
+  git clone https://github.com/ankitkhandal74/-yuji-itadori.git /-yuji-itadori
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
+  git clone $UPSTREAM_REPO /-yuji-itadori
 fi
-cd /TheMovieProviderBot
+cd /-yuji-itadori
 pip3 install -U -r requirements.txt
 echo "Starting TheMovieProviderBot...."
 python3 bot.py
