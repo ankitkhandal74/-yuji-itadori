980c085a3be26dfa596948842e620ce03dd77069
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2024-07-01 14:47:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '24512903'))
 API_HASH = environ.get('API_HASH', '7fd30d565055cc0f2f2460579202822a')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7033878453:AAGisp8B7hjhWjSsJP4u25GLhYHxh5xHKek")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -34,7 +34,7 @@ STREAM_API = (environ.get('STREAM_API', 'bf293c7351bfa196527926111d593fbd8e142d5
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/moviesearchbackupgroup00/25'))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6200938577').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002067009365').split()] #Channel id for auto indexing ( make sure bot is admin )
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
@@ -71,7 +71,7 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/DuduUpdatechannel')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/moviesearchbackupgroup00') # Tutorial video link for opening shortlink website 
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : Dudu_master')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002113321938')) #Log channel id ( make sure bot is admin )
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '')) #Log channel id ( make sure bot is admin )
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link ( make sure bot is admin )
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
